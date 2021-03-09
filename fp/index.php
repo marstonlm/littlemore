5de11b2aebba2877be607d302fbd5c7e1fa4d97d
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-03-09 07:21:33 -0800

    Moved final project
    Moved final project from 162 to here

--- fp/index.php
+++ fp/index.php
@@ -1,4 +1,17 @@
-<?php include "includes/header.php";?>
-<h1>Final Project</h1>
- 
-<?php include "includes/footer.php";?>
+<?php include "../includes/header.php";?>
+</div>
+
+<!-- START LEFT COL -->
+<section class="section-main">
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside class="aside-main">
+<h2></h2>
+
+</aside>
+<!-- END RIGHT COL -->
+
+<?php include "../includes/footer.php";?>
